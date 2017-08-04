356fb8e034255c33f373372d43a287b2a6fcba8f
= <=>
2017-08-04 14:19:36 +0800

    2017-9-4-pm

---
+++ median-of-two-sorted-arrays.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double findMedianSortedArrays(int *nums1, int num1Size, int *nums2, int num2Size) {
+	
+}
+
+int main()
+{
+	int nums1[] = {1, 3};
+	int nums2[] = {2};
+	printf("the median is %lf\n", findMedianSortedArrays(nums1, sizeof(nums1)/sizeof(nums1[0])), nums2, sizeof(nums2)/sizeof(nums2[0]));
+	return 0;
+}
